d67877e4f298baf37a4969c8eb06304f28829212
andrevaladares <andre.valadares@gmail.com>
2020-08-15 15:44:11 -0300

    1o commit do projeto. Criado componente base Adicionado css do bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DespesasComponent } from './despesas/despesas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DespesasComponent
   ],
   imports: [
     BrowserModule
